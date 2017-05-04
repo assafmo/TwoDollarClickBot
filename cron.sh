9b08f1d64aa50075025a478e8d246363935aecc3
assafmo <assaf.morami@gmail.com>
2017-05-04 23:32:30 +0300

    cron script

---
+++ cron.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+dir=$(dirname "$0")
+
+"$dir/go.sh" gfgqtmakia ***REMOVED*** 10
+"$dir/go.sh" gfgqtmakia ***REMOVED*** 20
+"$dir/go.sh" gfgqtmakia ***REMOVED*** 30
+"$dir/go.sh" gfgqtmakia ***REMOVED*** 40
+"$dir/go.sh" gfgqtmakia ***REMOVED*** 50
+"$dir/go.sh" gfgqtmakia ***REMOVED*** 60
+"$dir/go.sh" gfgqtmakia ***REMOVED*** 70
+"$dir/go.sh" gfgqtmakia ***REMOVED*** 80
+"$dir/go.sh" gfgqtmakia ***REMOVED*** 90
+"$dir/go.sh" gfgqtmakia ***REMOVED*** 100
+"$dir/go.sh" gfgqtmakia ***REMOVED*** 110
+"$dir/go.sh" gfgqtmakia ***REMOVED*** 120
+"$dir/go.sh" gfgqtmakia ***REMOVED*** 130
+"$dir/go.sh" gfgqtmakia ***REMOVED*** 140
+"$dir/go.sh" gfgqtmakia ***REMOVED*** 150
+"$dir/go.sh" gfgqtmakia ***REMOVED*** 160
+"$dir/go.sh" gfgqtmakia ***REMOVED*** 170
+"$dir/go.sh" gfgqtmakia ***REMOVED*** 180
+
+"$dir/go.sh" rami3232 ***REMOVED*** 10
+"$dir/go.sh" rami3232 ***REMOVED*** 20
+"$dir/go.sh" rami3232 ***REMOVED*** 30
+"$dir/go.sh" rami3232 ***REMOVED*** 40
+"$dir/go.sh" rami3232 ***REMOVED*** 50
+"$dir/go.sh" rami3232 ***REMOVED*** 60
+"$dir/go.sh" rami3232 ***REMOVED*** 70
+"$dir/go.sh" rami3232 ***REMOVED*** 80
+"$dir/go.sh" rami3232 ***REMOVED*** 90
+"$dir/go.sh" rami3232 ***REMOVED*** 100
+"$dir/go.sh" rami3232 ***REMOVED*** 110
+"$dir/go.sh" rami3232 ***REMOVED*** 120
+"$dir/go.sh" rami3232 ***REMOVED*** 130
+"$dir/go.sh" rami3232 ***REMOVED*** 140
+"$dir/go.sh" rami3232 ***REMOVED*** 150
+"$dir/go.sh" rami3232 ***REMOVED*** 160
+"$dir/go.sh" rami3232 ***REMOVED*** 170
+"$dir/go.sh" rami3232 ***REMOVED*** 180
