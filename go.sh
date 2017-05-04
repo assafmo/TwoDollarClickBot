da03432730d556b756ac0c2308ccff4f7d9eb238
assafmo <assaf.morami@gmail.com>
2017-05-04 21:35:29 +0300

    works!!!

--- go.sh
+++ go.sh
@@ -19,8 +19,18 @@ curl "https://www.twodollarclick.com/index.php?view=account&ac=click&sid=$sid&si
 while read path; do
 	iframe_path=$(curl "https://www.twodollarclick.com/$path" -H 'DNT: 1' -H 'Accept-Encoding: gzip, deflate, sdch, br' -H 'Accept-Language: en-US,en;q=0.8,he;q=0.6' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Mobile Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H "Referer: https://www.twodollarclick.com/index.php?view=account&ac=click&sid=$sid&sid2=$sid2&siduid=$siduid&" -H 'Connection: keep-alive' --compressed -s --cookie-jar /tmp/2dollar --cookie /tmp/2dollar | fgrep 'gpt.php' | awk -F '"' '{print $4}')
 	
-	curl "https://www.twodollarclick.com/$iframe_path" -H 'DNT: 1' -H 'Accept-Encoding: gzip, deflate, sdch, br' -H 'Accept-Language: en-US,en;q=0.8,he;q=0.6' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Mobile Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H "Referer: https://www.twodollarclick.com/$path" -H 'Connection: keep-alive' --compressed -s --cookie-jar /tmp/2dollar --cookie /tmp/2dollar
+	captch_html=$(curl "https://www.twodollarclick.com/$iframe_path" -H 'DNT: 1' -H 'Accept-Encoding: gzip, deflate, sdch, br' -H 'Accept-Language: en-US,en;q=0.8,he;q=0.6' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Mobile Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H "Referer: https://www.twodollarclick.com/$path" -H 'Connection: keep-alive' --compressed -s --cookie-jar /tmp/2dollar --cookie /tmp/2dollar)
+	
+	captch_key=$(echo "$captch_html" | awk -F '"' '/var key=/{print $(NF-1)}')
+	captch_index=$(echo "$captch_html" | fgrep '.png' | fgrep -n "$captch_key.png" | awk -F ':' '{print $1-1}')
 
-	exit 0
+	sleep "10.$RANDOM"
+
+	pretime=$(echo "$captch_html" | awk -F '"' '/var pretime=/{print $(NF-1)}')
+	id=$(echo "$captch_html" | awk -F '"' '/var id=/{print $(NF-1)}')
+
+	curl "https://www.twodollarclick.com/gpt.php?v=verify&buttonClicked=$captch_index&id=$id&type=ptc&pretime=$pretime&sid=$sid&sid2=$sid2&type=ptc&siduid=$siduid&" -H 'DNT: 1' -H 'Accept-Encoding: gzip, deflate, sdch, br' -H 'Accept-Language: en-US,en;q=0.8,he;q=0.6' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Mobile Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H "Referer: https://www.twodollarclick.com/gpt.php?v=timer&user=$1&pretime=$pretime&id=$id&sid=$sid&sid2=$sid2&type=ptc&siduid=$siduid&" -H 'Connection: keep-alive' --compressed -s --cookie-jar /tmp/2dollar --cookie /tmp/2dollar > /dev/null
+
+	curl "https://www.twodollarclick.com/index.php?view=account&ac=overview&sid=$sid&sid2=$sid2&siduid=$siduid&" -H 'DNT: 1' -H 'Accept-Encoding: gzip, deflate, sdch, br' -H 'Accept-Language: en-US,en;q=0.8,he;q=0.6' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Mobile Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H "Referer: https://www.twodollarclick.com/index.php?view=account&ac=click&sid=$sid&sid2=$sid2&siduid=$siduid&" -H 'Connection: keep-alive' --compressed -s --cookie-jar /tmp/2dollar --cookie /tmp/2dollar | fgrep 'overview-box-value' | head -1 | awk -F '[<>]' '{print $(NF-2)}'
 done < /tmp/2dollar_links
 
